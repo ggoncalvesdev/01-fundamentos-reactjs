367e4280c739209f83ae42fa47174096d0e9e796
Gabriel Gonçalves <gabriel.goncalves@neki-it.com.br>
2023-02-02 12:53:38 -0300

    feat(Sidebar): starting

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,25 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1675186883824-a14b5c5d8da2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/ggoncalvesdev.png"
+        />
+        <strong>Gabriel Gonçalves</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar seu perfil</a>
+      </footer>
+    </aside>
+  );
+}
